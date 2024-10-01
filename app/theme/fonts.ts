08abce76de5cc224319fb5700349e53b3ecbb045
Sendiko <sarangtawon897@gmail.com>
2024-10-01 20:43:10 +0700

    enc(add): colors and fonts

---
+++ app/theme/fonts.ts
@@ -0,0 +1,21 @@
+import { Poppins } from 'next/font/google';
+
+export const poppins400 = Poppins({
+  subsets: ['latin'],
+  weight: ['400']
+})
+
+export const poppins500 = Poppins({
+  subsets: ['latin'],
+  weight: ['500']
+})
+
+export const poppins600 = Poppins({
+  subsets: ['latin'],
+  weight: ['600']
+})
+
+export const poppins700 = Poppins({
+  subsets: ['latin'],
+  weight: ['700']
+})
