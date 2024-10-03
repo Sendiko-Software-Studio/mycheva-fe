61f5bb1171b1655b8bb2c435624eab91867b2b05
Sendiko <sarangtawon897@gmail.com>
2024-10-03 12:56:08 +0700

    enc(add): login ui

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -1,27 +1,77 @@
+"use client";
+
 import Image from "next/image";
-import '@/app/(login)/styles.css'
-import { poppins500, poppins700 } from "../theme/fonts";
+import { poppins700 } from "../theme/fonts";
+import { useState } from "react";
+import axios from "axios";
+import axiosInstance from "../lib/axios";
+import { url } from "inspector";
+import { tree } from "next/dist/build/templates/app-page";
 
 export default function Home() {
-  return (
-    <div className="page m-0 p-0">
-      <div className="left" />
-      <div className="right bg-neutral-50">
-        <div className="image-container">
-          <Image src="/cheva.png" width={163} height={43} alt="logo cheva" />
-        </div>
-        <h1 className={`${poppins700.className} welcome`}>Selamat Datang!</h1>
-        <p>Masukkan Username dan Password untuk mengakses akun!</p>
+  // const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setIsloading] = useState(false);
 
-        <div className="form">
+  const handleLogin = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setError(null);
+    try {
+      setIsloading(!loading);
+      // const response = await axiosInstance.post(
+      //   '/login',
+      //   { username, password }
+      // );
+      console.log({ username, password });
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-start gap-4 h-screen px-32 ">
+
+      <Image src='/cheva.png' width={163} height={43} alt="logo chevalier" />
+      <h1 className={`${poppins700.className} text-neutral-950 text-4xl`}>
+        Selamat Datang!
+      </h1>
+      <p className="text-neutral-950">
+        Masukkan Username dan Password untuk mengakses akun
+      </p>
+
+      { loading && <p className="text-neutral-50 bg-primary-500 p-2 rounded-lg w-full">Loading...</p>}
+
+      <form onSubmit={handleLogin} method="post" className="w-full">
+        <div className="flex flex-col text-neutral-950 w-full">
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" className="username" placeholder="Masukkan Username"/>
+          <input
+            className="border-2 border-neutral-500 w-full p-4 rounded-md focus:border-primary-500 focus:outline-none focus:ring-0"
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Masukkan Username"
+            value={username}
+            onChange={(value) => setUsername(value.target.value)}
+          />
         </div>
-        <div className="form">
+        <div className="flex flex-col text-neutral-950 w-full my-4">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" className="username" placeholder="Masukkan Password"/>
+          <input
+            className="border-2 border-neutral-500 w-full p-4 rounded-md focus:border-primary-500 focus:outline-none focus:ring-0"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Masukkan Password"
+            value={password}
+            onChange={(value) => setPassword(value.target.value)}
+          />
         </div>
-      </div>
+        <button type="submit" className="text-neutral-50 bg-primary-500 w-full py-4 rounded-lg hover:bg-primary-700">
+          Masuk
+        </button>
+      </form>
     </div>
   );
 }
