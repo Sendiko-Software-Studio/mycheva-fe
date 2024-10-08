c361d81ac0491069e75185148b15aa465edf9093
Sendiko <rizkysendiko7@gmail.com>
2024-10-08 22:13:37 +0700

    enc(add): successfully make call

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -3,15 +3,12 @@
 import Image from "next/image";
 import { poppins700 } from "../theme/fonts";
 import { useState } from "react";
-import axios from "axios";
 import axiosInstance from "../lib/axios";
-import { url } from "inspector";
-import { tree } from "next/dist/build/templates/app-page";
 
 export default function Home() {
   // const router = useRouter();
-  const [name, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const [loading, setIsloading] = useState(false);
 
@@ -20,22 +17,21 @@ export default function Home() {
     setError(null);
     try {
       setIsloading(!loading);
-      const response = await axiosInstance.post(
-        'login',
-        { "name": name, "password": password }
-      );
+      const response = await axiosInstance.post("login", {
+        name: name,
+        password: password,
+      });
       console.log(response);
     } catch (error: any) {
       setError(error.message);
       setIsloading(false);
       console.log(error.message);
     }
-  }
+  };
 
   return (
     <div className="flex flex-col justify-center items-start gap-4 h-screen lg:px-32 md:px-16 px-8">
-
-      <Image src='/cheva.png' width={163} height={43} alt="logo chevalier" />
+      <Image src="/cheva.png" width={163} height={43} alt="logo chevalier" />
       <h1 className={`${poppins700.className} text-neutral-950 text-4xl`}>
         Selamat Datang!
       </h1>
@@ -43,8 +39,16 @@ export default function Home() {
         Masukkan Username dan Password untuk mengakses akun
       </p>
 
-      { loading && <p className="text-neutral-50 bg-primary-500 p-2 rounded-lg w-full">Loading...</p>}
-      { error && <p className="text-neutral-50 bg-error p-2 rounded-lg w-full">{ error }</p>}
+      {loading && (
+        <p className="text-neutral-50 bg-primary-500 p-2 rounded-lg w-full">
+          Loading...
+        </p>
+      )}
+      {error && (
+        <p className="text-neutral-50 bg-error p-2 rounded-lg w-full">
+          {error}
+        </p>
+      )}
 
       <form onSubmit={handleLogin} method="post" className="w-full">
         <div className="flex flex-col text-neutral-950 w-full">
@@ -71,7 +75,10 @@ export default function Home() {
             onChange={(value) => setPassword(value.target.value)}
           />
         </div>
-        <button type="submit" className="text-neutral-50 bg-primary-500 w-full py-4 rounded-lg hover:bg-primary-700 focus:outline-primary-900">
+        <button
+          type="submit"
+          className="text-neutral-50 bg-primary-500 w-full py-4 rounded-lg hover:bg-primary-700 focus:outline-primary-900"
+        >
           Masuk
         </button>
       </form>
