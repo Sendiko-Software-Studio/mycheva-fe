de9e034b2b301ceb38c3c238d05efa9bf5f42fe0
Sendiko <sarangtawon897@gmail.com>
2024-10-21 14:53:08 +0700

    enc(add): fetch data, use props

--- app/components/announcement/card.tsx
+++ app/components/announcement/card.tsx
@@ -1,19 +1,21 @@
 import { poppins } from "@/app/theme/fonts";
 import Image from "next/image";
 
-export default function AnnouncementCard() {
+export default function AnnouncementCard(
+	{ imageUrl, title, content, time }: { imageUrl: string, title: string, content: string, time: string }
+) {
 	return(
 		<div className="flex flex-col lg:mx-24">
 			<div className={`${poppins} flex flex-row items-center gap-4`}>
-				<Image className="lg:w-20 lg:h-20 md:w-11 md:h-11 sm:w-10 w-9 sm:h-10 h-9 rounded-full object-cover object-center" src="/default-profile.jpeg" alt="user photo" width={0} height={0} sizes="100vw" />
+				<Image className="lg:w-20 lg:h-20 md:w-11 md:h-11 sm:w-10 w-9 sm:h-10 h-9 rounded-full object-cover object-center" src={imageUrl} alt="user photo" width={0} height={0} sizes="100vw" />
 				<div className="flex flex-col">
-					<h1 className={`${poppins} font-extrabold text-xl`}>Username</h1>
+					<h1 className={`${poppins} font-extrabold text-xl`}>{title}</h1>
 					<div className="p-1"></div>
-					<p className="text-neutral-400">24 jam yang lalu</p>
+					<p className="text-neutral-400">{time}</p>
 				</div>  
 			</div>
 			<div className="w-full bg-neutral-400 h-0.5 my-4"></div>
-			<p className="lg:text-l">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis sunt provident enim fugiat alias laudantium, distinctio, illum maxime placeat veritatis autem quia quaerat facilis deleniti minima sapiente deserunt, voluptatem vero!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam repellendus hic nihil cupiditate ipsum voluptatem accusamus assumenda velit impedit in possimus, ducimus esse sit! Itaque praesentium debitis non excepturi eius!</p>
+			<p className="lg:text-l">{content}</p>
 			<div className="w-full bg-neutral-400 h-0.5 my-4"></div>
 			<button className="self-end">
 				<Image src="/icon-bookmark.svg" width={24} height={24} alt="bookmark"/>
--- app/dashboard/announcement/page.tsx
+++ app/dashboard/announcement/page.tsx
@@ -1,19 +1,50 @@
+"use client"
 import AnnouncementCard from "@/app/components/announcement/card";  
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Announcements"
-}
+import axiosInstance from "@/app/lib/axios";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
 
 export default function AnnouncementHome() {
 
-  const announcements = []
+  const [announcements, setAnnouncements] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    const fetchData = async () => {
+      axiosInstance
+      .get(
+        "announcement",
+        { headers: { Authorization: `Bearer ${token}` }, withCredentials: true},
+      ).then((res) => {
+        console.log(res)
+        if (res.status == 200) {
+          setAnnouncements(res.data.announcements);
+          setLoading(false);
+        }
+      })
+      .catch((error: any) => {
+        console.log("error fetching: " + error.message)
+        setError(error);
+        setLoading(false);
+      })
+    }
+
+    fetchData();
+  }, [])
 
   return (
     <div className="h-screen gap-8">
-      {Array.from({ length: 5 }, (_, i) => (
-        <AnnouncementCard /> 
-      ))}
+      {loading && <p className={`transition-opacity duration-300 ease-in-out ${loading ? 'opacity-100' : 'opacity-0'} text-neutral-50 bg-primary-500 p-2 w-full rounded-lg`}>Loading...</p>}
+      {error && <p className={`transition-opacity duration-300 ease-in-out ${error ? 'opacity-100' : 'opacity-0'} text-neutral-50 bg-error p-2 rounded-lg w-full`}>{error}</p>}
+      {announcements && announcements.length > 0 ? (
+        announcements.map((announcement) => (
+          <AnnouncementCard key={announcement.id} title={announcement.title} content={announcement.content} imageUrl={announcement.user.profileUrl} time={announcement.createdAt} />
+        ))
+      ) : (
+        <div>No announcements available.</div>
+      )}
     </div>
   );
 }
