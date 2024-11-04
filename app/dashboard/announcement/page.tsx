d7607ad3693b4e3cbce4f6606b5526d72d27b220
Sendiko <sarangtawon897@gmail.com>
2024-11-04 11:13:19 +0700

    enc(add): card change

--- app/dashboard/announcement/page.tsx
+++ app/dashboard/announcement/page.tsx
@@ -22,6 +22,7 @@ export default function AnnouncementHome() {
         if (res.status == 200) {
           setAnnouncements(res.data.announcements);
           setLoading(false);
+          console.log(announcements);
         }
       })
       .catch((error: any) => {
@@ -35,12 +36,12 @@ export default function AnnouncementHome() {
   }, [])
 
   return (
-    <div className="h-screen gap-8">
+    <div className="h-screen gap-16">
       {loading && <p className={`transition-opacity duration-300 ease-in-out ${loading ? 'opacity-100' : 'opacity-0'} text-neutral-50 bg-primary-500 p-2 w-full rounded-lg`}>Loading...</p>}
       {error && <p className={`transition-opacity duration-300 ease-in-out ${error ? 'opacity-100' : 'opacity-0'} text-neutral-50 bg-error p-2 rounded-lg w-full`}>{error}</p>}
       {announcements && announcements.length > 0 ? (
         announcements.map((announcement) => (
-          <AnnouncementCard key={announcement.id} title={announcement.title} content={announcement.content} imageUrl={announcement.user.profileUrl} time={announcement.createdAt} />
+          <AnnouncementCard key={announcement.id} username={announcement.user.name} title={announcement.title} content={announcement.content} imageUrl={announcement.user.profileUrl} time={announcement.createdAt} />
         ))
       ) : (
         <div>No announcements available.</div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'external-content.duckduckgo.com'
+            }
+        ],
         domains: ['cdn-icons-png.flaticon.com'], // Add the domain of the external image
     }
 };
