b7b9078ea1293ceccdbe7e5ecec38d2f9a7574b5
Sendiko <sarangtawon897@gmail.com>
2024-11-24 23:27:37 +0700

    enc(add): props

--- app/components/calendar/event.tsx
+++ app/components/calendar/event.tsx
@@ -1,31 +1,39 @@
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { useState } from 'react';
 
-export default function MeetingCard() {
+export default function MeetingCard(
+  {title, time, date}: {title: string, time: string, date: string}
+) {
 
   const [isExpanded, setExpanded] = useState(false);
 
-  const handleExpaded = () => {
+  const handleExpanded = () => {
     setExpanded(!isExpanded);
   }
+  
+  const datePart = date.split("T")[0]; // "2024-12-12"
+
+  const [year, month, day] = datePart.split("-");
+  
+  const shortMonth = new Date(Number(year), parseInt(month) - 1).toLocaleString("en-US", {
+    month: "short",
+  });
+    const monthDay = `${shortMonth} ${day}`;
 
   return (
       <div className="flex flex-row p-4 h-fit w-100 rounded-xl gap-4 justify-start items-center">
-        <p className="text-lg font-semibold text-gray-500">19</p>     
-        <p className="text-lg font-normal text-gray-500">APR, Fri</p>
+        <p className="text-lg font-semibold text-gray-500">{monthDay}</p>
         <div className="flex flex-col w-full">
           <div className="flex flex-col bg-primary-300 p-4 rounded-xl w-full">
-            <div className="flex flex-row justify-between w-full">
-              <div className="size-6 bg-primary-500 rounded-md"/>
-              <p className="text-gray-700">10.00</p>
-              <p>Meet 1 Android Development</p>
-              <button onClick={handleExpaded}>
-                <ArrowDropDownIcon/>
-              </button>
-            </div>
-            <div className="flex flex-row pt-4">
-              <p>Title</p>
-              <p>Lorem ipsum dolor si amet</p>
+            <div className="flex flex-row justify-start gap-8 w-full">
+              <div className="flex flex-row gap-4">
+                <div className="size-6 bg-primary-500 rounded-md"/>
+                <p className="text-gray-700">{time}</p>
+              </div>
+              <p>{title}</p>
+              {/*<button onClick={handleExpanded}>*/}
+              {/*  <ArrowDropDownIcon/>*/}
+              {/*</button>*/}
             </div>
           </div>
         </div>
