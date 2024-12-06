639b80e4c73c0f3109105c1a2032e3b8ed0764a8
Sendiko <sarangtawon897@gmail.com>
2024-12-06 18:42:26 +0700

    enc(add): vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { poppins } from "./theme/fonts";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={`${poppins.className} min-h-screen`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
